89c535f312f7684775c99e125f5ff677f1db6982
jive_cc <jive_cc@5736e3ea-d426-0410-8aaf-93e336ac3372>
2006-11-08 12:56:42 +0000

    minor changes in the configuration files
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@20 5736e3ea-d426-0410-8aaf-93e336ac3372

---
+++ include/config.h
@@ -0,0 +1,17 @@
+/** 
+    Definitions of common types.
+    
+    Author: Nico Kruithof <Kruithof@jive.nl>
+**/
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "../config.h"
+
+// For using large files:
+#define _LARGEFILE_SOURCE
+#define _LARGEFILE64_SOURCE
+#define _FILE_OFFSET_BITS 64
+
+#endif // CONFIG_H
