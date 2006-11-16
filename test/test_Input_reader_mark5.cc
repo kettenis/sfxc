790810c897c949e5d9d7794e960f2392df6d9bf6
kruithof <kruithof@5736e3ea-d426-0410-8aaf-93e336ac3372>
2006-11-16 07:44:06 +0000

    Added a script for testing test_Input_reader_mark5
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@40 5736e3ea-d426-0410-8aaf-93e336ac3372

--- test/test_Input_reader_mark5.cc
+++ test/test_Input_reader_mark5.cc
@@ -4,17 +4,20 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <unistd.h>
 
-char *infile = "data/input.txt";
-char *outfile = "output.txt";
 #define BUFFSIZE 1000
 
 int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << "usage: " << argv[0] << " <output_file>" << std::endl;
+    exit(1);
+  }
   int nBytes;
   char buff[BUFFSIZE];
 
   Input_reader_mark5 reader;
-  std::ofstream out(outfile, std::ios::out | std::ios::binary);
+  std::ofstream out(argv[1], std::ios::out | std::ios::binary);
 
   nBytes= reader.get_bytes(100, buff);
   assert(nBytes==100);
@@ -42,25 +45,15 @@ int main(int argc, char *argv[]) {
   nBytes = reader.move_forward(BUFFSIZE);
   assert (nBytes == BUFFSIZE);
 
-  while ((nBytes = reader.get_bytes(BUFFSIZE, buff)) > 0) {
-    out.write(buff, nBytes*sizeof(char));
-    nBytes = reader.move_forward(BUFFSIZE);
-    //assert (nBytes == BUFFSIZE);
+  int nGet, nForward;
+  while ((nGet = reader.get_bytes(BUFFSIZE, buff)) > 0) {
+    out.write(buff, nGet*sizeof(char));
+    nForward = reader.move_forward(nGet);
+    assert (nGet == nForward);
+    usleep(10);
   }
 
   out.close();
-  
-  //check output:
-  std::string command = "diff ";
-  command += infile; command += " "; command += outfile;
-  int result = system(command.c_str());
-
-  if (result != 0) {
-    std::cout << "ERROR: Difference in files" << std::endl;
-    exit(1);
-  }
-  
-  remove(outfile);
 
   return 0;
 }
