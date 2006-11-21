7df08d3b5035b1a732b2c1a02d8a7847467b7666
oerleman <oerleman@5736e3ea-d426-0410-8aaf-93e336ac3372>
2006-11-21 10:20:53 +0000

    delayTable.h new include for handling delay tables : ----------------------------------------------------------------------
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@43 5736e3ea-d426-0410-8aaf-93e336ac3372

---
+++ include/delayTable.h
@@ -0,0 +1,53 @@
+/**
+CVS keywords
+$Author $
+$Date$
+$Name$
+$Revision$
+$Source$
+
+Class definitions for delay table
+
+Author     : RHJ Oerlemans
+StartDate  : 20061115
+Last change: 20061115
+
+**/
+
+
+class delayTable
+{
+
+  private:
+
+    //
+    ITN64    ndel; // number of parabolic delay functions covering start until stop
+    
+    INT64  *tdt;          // time in delaytable
+    INT64  dt;            // time step in delaytable
+    double *cA, *cB, *cC; // Y = aX^2 + bX + c
+    double *mA, *mB, *mC; // per read delay line one series of these coefficients
+    double *rA, *rB, *rC;
+    double *fA, *fB, *fC;
+    
+        
+  public:
+
+    //default constructor, set default values 
+    delayTable();
+
+    //destructor
+    ~delayTable();
+    
+    //read the delay table, do some checks and
+    //calculate coefficients for parabolic interpolation
+    int readDelayTable(char *delayTableName, INT64 start, INT64 stop);
+
+    //calculate the delay for the delayType at time in microseconds
+    double calcDelay(INT64 time, int delayType);
+
+    enum delayType {Cdel, Mdel, Rdel, Fdel};
+
+};
+
+
