68dc1584bc163efcad97f310805f540b9de843fc
kruithof <kruithof@5736e3ea-d426-0410-8aaf-93e336ac3372>
2006-12-20 12:03:55 +0000

    Added small program for creating the delay tables from coarse data points.
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@58 5736e3ea-d426-0410-8aaf-93e336ac3372

---
+++ utils/create_delay_function.cc
@@ -0,0 +1,65 @@
+/*
+$Author$
+$Date$
+$Name$
+$Revision$
+$Source$
+
+  This small utility reads data from a file (argv[1]), interpolates
+  the data and writes it to file (argv[2]).
+
+  You will need gsl (GNU scientific library) for the interpolation.
+
+*/
+
+#include <assert.h>
+#include <math.h>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_spline.h>
+#include <fstream>
+#include <stdio.h>
+#include <vector>
+
+#define N 25
+
+int
+main (int argc, char *argv[])
+{
+  assert(argc == 3);
+  std::ifstream infile(argv[1]);
+  FILE *outfile = fopen(argv[2], "w");
+
+  double datax[25], datay[25];
+
+  int rows = 0;
+  // read data: (9 columns for n05c2)
+  double c1,c2,c3,c4,c5,c6,c7,c8,c9;
+  while (infile >> c1 >> c2 >> c3 >> c4 >> c5 >> c6 >> c7 >> c8 >> c9) {
+    assert(rows <= N);;
+    datax[rows] = c1;
+    datay[rows] = c2;
+    rows++;
+  }
+
+  { // Do the interpolation
+    gsl_interp_accel *acc 
+      = gsl_interp_accel_alloc ();
+    gsl_spline *spline 
+      = gsl_spline_alloc (gsl_interp_cspline, rows);
+    
+    gsl_spline_init (spline, datax, datay, rows);
+    
+    for (double xi = datax[0]-3; xi < datax[rows-1]+3; xi += 0.125)
+      {
+        double yi = gsl_spline_eval (spline, xi, acc);
+
+        // Print with high precision:
+        fprintf (outfile, "%f %20.16f 0.0 0.0\n", xi, yi);
+        //outfile << xi << " " << yi << " 0.0 0.0" << std::endl;
+      }
+    gsl_spline_free (spline);
+    gsl_interp_accel_free(acc);
+  }
+  return 0;
+}
+
