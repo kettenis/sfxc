1e25d7b2f1fe54482947b5fcb766230a55fbea0a
kruithof <kruithof@5736e3ea-d426-0410-8aaf-93e336ac3372>
2006-12-20 13:03:14 +0000

    Reading the timestamp correctly now (hhmmss)
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@59 5736e3ea-d426-0410-8aaf-93e336ac3372

--- utils/create_delay_function.cc
+++ utils/create_delay_function.cc
@@ -16,6 +16,7 @@ $Source$
 #include <math.h>
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_spline.h>
+#include <iostream>
 #include <fstream>
 #include <stdio.h>
 #include <vector>
@@ -25,19 +26,34 @@ $Source$
 int
 main (int argc, char *argv[])
 {
-  assert(argc == 3);
+  if (argc != 3) {
+    std::cout << "Usage: " << argv[0] << " <infile> <outfile>" << std::endl;
+  }
+
   std::ifstream infile(argv[1]);
+  assert(infile.is_open());
   FILE *outfile = fopen(argv[2], "w");
+  assert(outfile != 0);
 
   double datax[25], datay[25];
 
   int rows = 0;
   // read data: (9 columns for n05c2)
-  double c1,c2,c3,c4,c5,c6,c7,c8,c9;
+  int c1;
+  double c2,c3,c4,c5,c6,c7,c8,c9;
   while (infile >> c1 >> c2 >> c3 >> c4 >> c5 >> c6 >> c7 >> c8 >> c9) {
-    assert(rows <= N);;
-    datax[rows] = c1;
-    datay[rows] = c2;
+    assert(rows <= N);
+    // c1 is hhmmss format, get the number of seconds from 24:00h:
+    int sec = c1%100; // Seconds
+    sec += ((c1/100)%100)*60; // Minutes
+    sec += ((c1/10000)%100)*60*60; // Hours
+    
+    datax[rows] = sec;
+    datay[rows] = c8;
+
+    // Print out data for debugging purposes:
+    printf ("%d %20.16f\n", sec, datay[rows]);
+
     rows++;
   }
 
@@ -49,14 +65,14 @@ main (int argc, char *argv[])
     
     gsl_spline_init (spline, datax, datay, rows);
     
-    for (double xi = datax[0]-3; xi < datax[rows-1]+3; xi += 0.125)
-      {
-        double yi = gsl_spline_eval (spline, xi, acc);
+    for (double xi = datax[0]-3; xi < datax[rows-1]+3; xi += 0.125) {
+      double yi = gsl_spline_eval (spline, xi, acc);
+      
+      // Print with high precision:
+      fprintf (outfile, "%f %20.16f 0.0 0.0\n", xi, yi/1000000);
+      //outfile << xi << " " << yi << " 0.0 0.0" << std::endl;
+    }
 
-        // Print with high precision:
-        fprintf (outfile, "%f %20.16f 0.0 0.0\n", xi, yi);
-        //outfile << xi << " " << yi << " 0.0 0.0" << std::endl;
-      }
     gsl_spline_free (spline);
     gsl_interp_accel_free(acc);
   }
