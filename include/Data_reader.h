0720e43acd2403048b7f7d64ff6507997b813995
kruithof <kruithof@5736e3ea-d426-0410-8aaf-93e336ac3372>
2007-01-02 13:38:21 +0000

    Forgot Data_reader.h
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@63 5736e3ea-d426-0410-8aaf-93e336ac3372

---
+++ include/Data_reader.h
@@ -0,0 +1,40 @@
+/*
+CVS keywords
+$Author$
+$Date$
+$Name$
+$Revision$
+$Source$
+*/
+
+#ifndef DATA_READER_H
+#define DATA_READER_H
+
+#include <types.h>
+
+/** Virtual class defining the interface for obtaining input.
+ **/
+class Data_reader {
+public:
+  virtual ~Data_reader() {
+  }
+//   /** Moves the read_pointer nBytes bytes forward. 
+//       @param[in] nBytes Number of bytes to advance.
+//       \pre nBytes >= 0
+//       \return Returns the number of bytes advanced.
+//   **/
+  /// TODO: NGHK: REMOVE
+  UINT64 move_forward(UINT64 nBytes) { return nBytes; };
+
+  /** Reads nBytes from channel into the buff starting from the
+      read_pointer. If buff == NULL, then the buffer is not 
+      filled and the amount of bytes is only read.
+      
+      \return the number of bytes read into the buffer.
+      \pre buff is allocated. nBytes >= 0
+  **/ 
+  virtual UINT64 get_bytes(UINT64 nBytes, char *buff) = 0;
+};
+
+
+#endif // DATA_READER_H
