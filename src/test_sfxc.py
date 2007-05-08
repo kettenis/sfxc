9e16207aa1f1d9e9c18834d04d10bfb9c01541fa
kruithof <kruithof@5736e3ea-d426-0410-8aaf-93e336ac3372>
2007-05-08 06:59:48 +0000

    Extended the test script with command line arguments.
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@216 5736e3ea-d426-0410-8aaf-93e336ac3372

--- src/test_sfxc.py
+++ src/test_sfxc.py
@@ -8,7 +8,27 @@
 # $Id$
 # 
 
-import sys, os,time, filecmp;
+import sys, os,time, filecmp, getopt;
+
+numProcesses = 5
+
+def usage():
+  print sys.argv[0]
+  print "  -n   d"
+  print "  --np d: number of processes"
+
+try:
+  opts, args = getopt.getopt(sys.argv[1:], "hn:", ["help", "np="])
+except getopt.GetoptError:
+  usage()
+  sys.exit(2)
+
+for opt, arg in opts:
+  if opt in ("-h", "--help"):
+    usage()
+    sys.exit(2)
+  if opt in ("-n", "--np"):
+    numProcesses = arg;
 
 # Load the ccf files for testing:
 RC_FILE = os.path.join(os.environ.get('HOME'), ".sfxcrc")
@@ -22,7 +42,7 @@ if (status != 0): sys.exit(1)
 # run the executable on all ccf files
 for ctrlfile in controlfiles:
   print "Control file: "+ctrlfile
-  status = os.system("time mpirun -np 10 sfxc "+ctrlfile)
+  status = os.system("time mpirun -np "+numProcesses+" sfxc "+ctrlfile)
   if (status != 0): sys.exit(1)
   
 sys.exit(0);
