8e08a2382482572e9af4419e1acc309f4fedb7b6
kruithof <kruithof@5736e3ea-d426-0410-8aaf-93e336ac3372>
2008-03-31 07:48:07 +0000

    Mac2unix endline conversion
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@693 5736e3ea-d426-0410-8aaf-93e336ac3372

--- include/channel_extractor_fast.h
+++ include/channel_extractor_fast.h
@@ -1,5 +1,21 @@
-/* Copyright (c) 2007 Joint Institute for VLBI in Europe (Netherlands) * Copyright (c) 2007 University of Amsterdam (Netherlands) * All rights reserved. * * Author(s): Nico Kruithof <Kruithof@JIVE.nl>, 2007 *            Damien Marchal <dmarchal@science.uva.nl>, 2007 * * * This file is part of: *   - SFXC/SCARIe project. * This file contains: *   - declaration of the Channel_extrator_fast class. */
-#ifndef CHANNEL_EXTRACTOR_FAST_H__#define CHANNEL_EXTRACTOR_FAST_H__#include "channel_extractor_interface.h"
+/* Copyright (c) 2007 Joint Institute for VLBI in Europe (Netherlands)
+ * Copyright (c) 2007 University of Amsterdam (Netherlands)
+ * All rights reserved.
+ *
+ * Author(s): Nico Kruithof <Kruithof@JIVE.nl>, 2007
+ *            Damien Marchal <dmarchal@science.uva.nl>, 2007
+ *
+ *
+ * This file is part of:
+ *   - SFXC/SCARIe project.
+ * This file contains:
+ *   - declaration of the Channel_extrator_fast class.
+ */
+#ifndef CHANNEL_EXTRACTOR_FAST_H__
+#define CHANNEL_EXTRACTOR_FAST_H__
+
+#include "channel_extractor_interface.h"
+
 /*******************************************************************************
 *
 * @class Channel_extractor_fast
@@ -10,4 +26,28 @@
 * On DAS3 cut the number by 2 to get a rule of thumb approximation. Don't
 * be worried by the compile time. Everything that is done at compiled time...
 * will not to be done at run time :)
-*******************************************************************************/class Channel_extractor_fast : public Channel_extractor_interface {public:  Channel_extractor_fast();  void initialise(const std::vector< std::vector<int> > &track_positions_,                  int size_of_one_input_word_,                  int input_sample_size_);  void extract(unsigned char *in_data1,               unsigned char **output_data);private:  Channel_extractor_interface* hidden_implementation_;  std::vector< std::vector<int> > track_positions;  int size_of_one_input_word;  int input_sample_size;  int n_subbands;  // Computed  int fan_out;};#endif // CHANNEL_EXTRACTOR_FAST_H__
+*******************************************************************************/
+class Channel_extractor_fast : public Channel_extractor_interface {
+public:
+  Channel_extractor_fast();
+
+  void initialise(const std::vector< std::vector<int> > &track_positions_,
+                  int size_of_one_input_word_,
+                  int input_sample_size_);
+
+  void extract(unsigned char *in_data1,
+               unsigned char **output_data);
+private:
+  Channel_extractor_interface* hidden_implementation_;
+
+  std::vector< std::vector<int> > track_positions;
+  int size_of_one_input_word;
+  int input_sample_size;
+  int n_subbands;
+
+  // Computed
+  int fan_out;
+};
+
+#endif // CHANNEL_EXTRACTOR_FAST_H__
+
