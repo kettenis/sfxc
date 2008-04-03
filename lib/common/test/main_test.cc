ac39c84778b32915eac787e8dae04a973055efa5
marchal <marchal@5736e3ea-d426-0410-8aaf-93e336ac3372>
2008-04-03 11:56:45 +0000

    Compiling with -DENABLE_TEST_UNIT permit to use and generate the unitary test unit for the libraries.
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@717 5736e3ea-d426-0410-8aaf-93e336ac3372

--- lib/common/test/main_test.cc
+++ lib/common/test/main_test.cc
@@ -1,24 +1,17 @@
 #include <iostream>
 
-#ifdef ENABLE_TEST_UNIT
-
 #include "Test_unit.h"
 #include "backtrace.h"
 #include "demangler.h"
 
 int main(int argc, char** argv)
 {
+std::cout << "Starting tests" << std::endl;	
+
+#ifdef ENABLE_TEST_UNIT
 	Test_manager manager;
-
-	std::cout << "Starting tests" << std::endl;
-	manager.add_test( new Backtrace::Test() );
-
+	//manager.add_test( new Backtrace::Test() );
 	manager.do_test();
+#endif //
+	
 }
-#else
-int main(int argc, char** argv)
-{
-		std::cout << "bootstrap version, test are disabled" << std::endl;
-}
-#endif //ENABLE_TEST_UNIT
-
