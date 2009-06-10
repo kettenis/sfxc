bd9ed218789ac4b84022e798c66c0e961aca388c
sipior <sipior@5736e3ea-d426-0410-8aaf-93e336ac3372>
2009-06-10 12:48:42 +0000

    Backporting correlator changes from last branch to present. Hopefully I've managed not to fuck it up.
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@972 5736e3ea-d426-0410-8aaf-93e336ac3372

--- include/delay_correction_default.h
+++ include/delay_correction_default.h
@@ -12,6 +12,7 @@
 #define DELAY_CORRECTION_DEFAULT_H
 #include <boost/shared_ptr.hpp>
 #include <complex>
+#include <cufft.h>
 #include <fftw3.h>
 
 #include "delay_correction_base.h"
@@ -33,13 +34,14 @@ public:
 
 private:
   ///
-  void fractional_bit_shift(FLOAT input[],
+  void fractional_bit_shift(cufftReal* input,
                             int integer_shift,
                             FLOAT fractional_delay);
   void fringe_stopping(FLOAT output[]);
 
 private:
-  FFTW_PLAN       plan_t2f, plan_f2t, plan_t2f_cor;
+  cufftHandle       plan_t2f, plan_f2t, plan_t2f_cor;
+  //MSS FFTW_PLAN       plan_t2f, plan_f2t, plan_t2f_cor;
   Memory_pool_vector_element<FLOAT >                 plan_input_buffer;
   Memory_pool_vector_element< std::complex<FLOAT> >  plan_output_buffer;
 
