192f33093b3a8636be26ea4d00f445779267cfe5
keimpema <keimpema@5736e3ea-d426-0410-8aaf-93e336ac3372>
2011-05-31 12:48:21 +0000

    Fix pulsar correlation
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@1158 5736e3ea-d426-0410-8aaf-93e336ac3372

--- src/correlation_core_pulsar.cc
+++ src/correlation_core_pulsar.cc
@@ -26,6 +26,12 @@ Correlation_core_pulsar::set_parameters(const Correlation_parameters &parameters
   if (input_elements.size() != number_input_streams_in_use()) {
     input_elements.resize(number_input_streams_in_use());
   }
+
+  if (input_conj_buffers.size() != number_input_streams_in_use()) {
+    input_conj_buffers.resize(number_input_streams_in_use());
+    for(int i=0;i<number_input_streams_in_use();i++)
+      input_conj_buffers[i].resize(fft_size() + 1);
+  }
   n_flagged.resize(baselines.size());
 
   double start_mjd = parameters.start_time.get_mjd();
