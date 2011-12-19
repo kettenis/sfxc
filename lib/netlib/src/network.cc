adcd28749056453bf3c2051ca3cfab121bb6e740
kettenis <kettenis@5736e3ea-d426-0410-8aaf-93e336ac3372>
2011-12-19 11:11:49 +0000

    Remove dead code and clean it up thoroughly.
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@1298 5736e3ea-d426-0410-8aaf-93e336ac3372

--- lib/netlib/src/network.cc
+++ lib/netlib/src/network.cc
@@ -6,167 +6,104 @@
 #include <ifaddrs.h>
 #include <netdb.h>
 
-#include <unistd.h>
-
 #include <iostream>
 
 #include "network.h"
-
 #include "exception_common.h"
-
 #include "interface.h"
-
 #include "connexion.h"
 
-
-
-
-void Network::get_interfaces_ordered_by_name(const Vector_string& ifname, std::vector<InterfaceIP*>& ifs) {
-  /// We retreive the list of network interfaces
-  std::vector<InterfaceIP*> interfaces;
+void
+Network::get_interfaces_ordered_by_name(const Vector_string& names,
+					std::vector<InterfaceIP*>& interfaces)
+{
+  std::vector<InterfaceIP*> unsorted_interfaces;
   std::vector<bool> selected;
 
-  /// Get the list of interfaces...
-  Network::get_interfaces(interfaces);
+  Network::get_interfaces(unsorted_interfaces);
 
-  for (unsigned int i=0;i<interfaces.size();i++)
+  for (size_t i = 0; i < interfaces.size(); i++)
     selected.push_back(false);
 
-  /// order the given interface
-  for (unsigned int i=0;i<ifname.size();i++) {
-    for (unsigned int j=0;j<interfaces.size();j++) {
-      if ( ifname[i].find( interfaces[j]->name() ) != String::npos  ||
-           ifname[i].find( interfaces[j]->ip() ) != String::npos ) {
-        ifs.push_back( interfaces[j] );
+  // Order the interfaces given by NAMES.
+  for (size_t i = 0; i < names.size(); i++) {
+    for (size_t j = 0; j < unsorted_interfaces.size(); j++) {
+      if (names[i].find(unsorted_interfaces[j]->name()) != String::npos ||
+	  names[i].find(unsorted_interfaces[j]->ip()) != String::npos ) {
+        interfaces.push_back(unsorted_interfaces[j]);
         selected[j] = true;
       }
     }
   }
 
-  /// add the remaining ones.
-  for (unsigned int i=0;i<interfaces.size();i++) {
-    if ( !selected[i] )
-      ifs.push_back( interfaces[i] );
+  // Add any remaining interfaces.
+  for (size_t i = 0; i < unsorted_interfaces.size(); i++) {
+    if (!selected[i])
+      interfaces.push_back(unsorted_interfaces[i]);
   }
 }
 
+InterfaceIP*
+Network::get_interface_by_name(const std::string& name)
+{
+  std::vector<InterfaceIP*> interfaces;
 
+  Network::get_interfaces(interfaces);
 
-InterfaceIP* Network::get_interface_by_name(const std::string& name) {
+  for (size_t i = 0; i < interfaces.size(); i++) {
+    if (interfaces[i]->name().find(name) != String::npos)
+      return interfaces[i];
 
-  std::vector<InterfaceIP*> interface;
-
-
-
-  Network::get_interfaces(interface);
-
-  for (unsigned int i=0;i<interface.size();i++) {
-
-    if ( interface[i]->name().find( name ) != String::npos ) return interface[i];
-
-    if ( interface[i]->ip().find( name ) != String::npos ) return interface[i];
+    if (interfaces[i]->ip().find(name) != String::npos)
+      return interfaces[i];
   }
 
-
   return NULL;
-
 }
-
-
-
-InterfaceIP* Network::get_first_interface() {
-
-  std::vector<InterfaceIP*> interface;
-
-  Network::get_interfaces(interface);
-
-  if ( interface.size() != 0 ) return interface[0];
-
-  return NULL;
-
-}
-
-
 
 pInterfaceIP Network::interface_any_ = NULL;
 
-InterfaceIP* Network::get_any_interface() {
-
-  if ( interface_any_ == NULL )
-
+InterfaceIP*
+Network::get_any_interface()
+{
+  if (interface_any_ == NULL)
     interface_any_ = new InterfaceIP("any", "any");
 
-
-
   return interface_any_;
-
 }
 
+void
+Network::get_interfaces(std::vector<InterfaceIP*>& interfaces)
+{
+  struct ifaddrs *ifa, *ifaddr;
 
-
-void Network::get_interfaces(std::vector<InterfaceIP*>& vectorinterfaces) {
-
-  struct ifaddrs *ifa = NULL;
-
-
-
-  if (getifaddrs (&ifa) < 0) {
-
-    std::cout << "error in getifaddrs" << std::endl;
-
+  if (getifaddrs (&ifaddr) == -1)
     return;
 
-  }
-
-
-
-  for (; ifa; ifa = ifa->ifa_next) {
-
-    char ip[ NI_MAXHOST ];
-
-    socklen_t salen;
-
-
+  for (ifa = ifaddr; ifa; ifa = ifa->ifa_next) {
+    if (ifa->ifa_addr->sa_family != AF_INET)
+      continue;
     if (ifa->ifa_flags & IFF_LOOPBACK)
       continue;
 
-
-    //std::cout << "scanning network interface:" << ifa->ifa_name << std::endl;
-
-    if (ifa->ifa_addr->sa_family == AF_INET) {
-
-      // IP v4
-
-      salen = sizeof (struct sockaddr_in);
-
-      if (getnameinfo (ifa->ifa_addr, salen,
-
-                       ip, sizeof (ip), NULL, 0, NI_NUMERICHOST) < 0) {
-
-        perror ("getnameinfo");
-
-        continue;
-
-      }
-
-      std::string name = ifa->ifa_name;
-
-      std::string address = ip;
-
-      vectorinterfaces.push_back( new InterfaceIP(name, address) );
-
+    char ip[NI_MAXHOST];
+    if (getnameinfo (ifa->ifa_addr, sizeof(struct sockaddr_in),
+		     ip, sizeof (ip), NULL, 0, NI_NUMERICHOST) < 0) {
+      perror ("getnameinfo");
+      continue;
     }
 
+    std::string name = ifa->ifa_name;
+    std::string address = ip;
+    interfaces.push_back(new InterfaceIP(name, address));
   }
 
-
-
   freeifaddrs (ifa);
-
 }
 
-
-bool Network::match_interface(in_addr_t ip) {
+bool
+Network::match_interface(in_addr_t ip)
+{
   struct ifaddrs *ifa, *ifaddr;
   in_addr_t addr, mask;
 
@@ -188,70 +125,20 @@ bool Network::match_interface(in_addr_t ip) {
   return false;
 }
 
-
-InterfaceIP* Network::scan_interfaces() {
-
-  // Scanning available network interfaces
-
-  InterfaceIP *interface = NULL;
-
-  interface = Network::get_interface_by_name("myri0");
-
-  if ( !interface ) {
-
-    interface = Network::get_interface_by_name("eth0");
-
-    if ( !interface ) {
-
-      interface =Network::get_first_interface();
-
-      if ( !interface) MTHROW("Unable to find a suitable network interface");
-
-    }
-
-  }
-
-  return interface;
-
-}
-
-
-
-
-
-InterfaceIP* Network::get_interface_by_preffered(Vector_string& prefferedif) {
-
-  std::vector<InterfaceIP*> interface;
-
-
-
-  Network::get_interfaces(interface);
-
-  for (unsigned int j =0;j<prefferedif.size();j++ ) {
-
-    String& name = prefferedif[j];
-
-    for (unsigned int i=0;i<interface.size();i++) {
-
-      if ( interface[i]->name().find( name ) != String::npos ) return interface[i];
-
-    }
-
-  }
-
-  return NULL;
-
-}
-
-pConnexion Network::connect_to(const String& ipaddress, unsigned short port, int type) {
+pConnexion
+Network::connect_to(const String& ipaddress, unsigned short port, int type)
+{
   return get_any_interface()->connect_to(ipaddress, port, type);
 }
 
-
-pConnexion Network::connect_to(uint64_t ip, short port, int type) {
+pConnexion
+Network::connect_to(uint64_t ip, short port, int type)
+{
   return get_any_interface()->connect_to(ip, port, type);
 }
 
-EndpointIP* Network::create_endpoint(unsigned short port) {
+EndpointIP*
+Network::create_endpoint(unsigned short port)
+{
   return get_any_interface()->create_endpoint(port);
 }
--- lib/netlib/src/network.h
+++ lib/netlib/src/network.h
@@ -1,11 +1,7 @@
 #ifndef NETWORK_HH
-
 #define NETWORK_HH
 
-
-
 #include <vector>
-
 #include <string>
 
 #include "tcp_connection.h"
@@ -20,36 +16,23 @@
 
 class Network {
 public:
-  static void get_interfaces(std::vector<InterfaceIP*>& intf);
+  static void get_interfaces(std::vector<InterfaceIP*>& interfaces);
   static bool match_interface(in_addr_t dest);
 
-  /// Return a list of interfaces, ordered by the given list of name
-  static void get_interfaces_ordered_by_name(const Vector_string& prefferedif,
-      std::vector<InterfaceIP*>& resif);
+  // Return a list of interfaces, ordered by the given list of interface names.
+  static void get_interfaces_ordered_by_name(const Vector_string& names,
+      std::vector<InterfaceIP*>& interfaces);
 
   static pInterfaceIP get_interface_by_name(const String& name);
-  static pInterfaceIP get_interface_by_preffered(Vector_string& prefferedif);
-  static pInterfaceIP get_first_interface();
-
   static pInterfaceIP get_any_interface();
 
-  static pInterfaceIP scan_interfaces();
+  static pConnexion connect_to(const std::string& ipaddress, unsigned short port, int type = SOCK_STREAM);
+  static pConnexion connect_to(uint64_t ip, short port, int type = SOCK_STREAM);
 
-  static pConnexion connect_to(const std::string& ipaddress, unsigned short port, int type=SOCK_STREAM);
-  static pConnexion connect_to(uint64_t ip, short port, int type=SOCK_STREAM);
-
-  static EndpointIP* create_endpoint(unsigned short port=0);
-protected:
+  static EndpointIP* create_endpoint(unsigned short port = 0);
 
 private:
-
   static pInterfaceIP interface_any_;
-
 };
 
-
-
-
-
 #endif // NETWORK_HH
-
