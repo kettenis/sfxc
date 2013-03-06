f797dbc90a7cfc564cae74b6e057b43f4cc92f54
kettenis <kettenis@5736e3ea-d426-0410-8aaf-93e336ac3372>
2013-03-06 15:35:42 +0000

    Sanitize Input_data_format_reader::find_fill_pattern().  Previous version fixed a long standing issue where we'd incorrectly mark a single word of data as invalid, but might ignore the case where the last word in a frame is marked invalid.  Probably should never happen, but this version does it right and is easier to understand (famous last words...).
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@1508 5736e3ea-d426-0410-8aaf-93e336ac3372

--- src/input_data_format_reader.cc
+++ src/input_data_format_reader.cc
@@ -14,55 +14,59 @@ bool Input_data_format_reader::eof() {
 
 void Input_data_format_reader::find_fill_pattern(Data_frame &data){
   int buffer_size = data.buffer->data.size() / 4; // number of 32 bit words in buffer
-  uint32_t *buffer=(uint32_t *)&data.buffer->data[0];
+  uint32_t *buffer = (uint32_t *)&data.buffer->data[0];
 
   // See if there is already a bit of invalid data (assumed to start at byte 0)
-  int end_previous_invalid = 0;
-  if(data.invalid.size() > 0)
-    end_previous_invalid = data.invalid[0].nr_invalid / 4;
+  int start = 0;
+  if (data.invalid.size() > 0) {
+    SFXC_ASSERT(data.invalid.size() == 1);
+    SFXC_ASSERT(data.invalid[0].invalid_begin == 0);
+    start = data.invalid[0].nr_invalid / 4;
+  }
 
-  // If the first word contains fill pattern search for the end
-  int start = end_previous_invalid;
-  int end = start; 
-  while(start < buffer_size -1){
-    // find the start of the fill pattern
-    if(buffer[start] != MARK5_FILLPATTERN){
+  while (start < buffer_size) {
+    // Find the start of the fill pattern
+    if (buffer[start] != MARK5_FILLPATTERN) {
       int step = std::min(MARK5_FILLPATTERN_NWORDS, buffer_size - start - 1);
-      while(step > 0){
-        if(buffer[start + step] != MARK5_FILLPATTERN){
+      while (step > 0) {
+        if (buffer[start + step] != MARK5_FILLPATTERN) {
           start += step;
-          if(start + step >= buffer_size)
-            step = buffer_size - start - 1;
-        }else{
+	  step = std::min(step, buffer_size - start - 1);
+        } else {
           step /= 2;
         }
       }
-      if (start < buffer_size -1)
-        start += 1;
+      // Make sure we point at the first word containing fill pattern
+      // (or past the end of the buffer) and not at the last word that
+      // does no contain fill pattern
+      start += 1;
     }
-    if (start < buffer_size -1){ 
-      // Find the end of the fill pattern
-      end = start;
-      int step = std::min(MARK5_FILLPATTERN_NWORDS, buffer_size - start - 1);
-      while(step > 0){
-        if(buffer[end + step] == MARK5_FILLPATTERN){
-          end += step;
-          if(end + step >= buffer_size)
-            step = buffer_size - end - 1;
-        }else{
-          step /= 2;
-        }
+
+    // Bail if we didn't find any fill pattern
+    if (start >= buffer_size)
+      break;
+
+    // Find the end of the fill pattern
+    int end = start;
+    int step = std::min(MARK5_FILLPATTERN_NWORDS, buffer_size - end - 1);
+    while (step > 0) {
+      if (buffer[end + step] == MARK5_FILLPATTERN) {
+        end += step;
+	step = std::min(step, buffer_size - end - 1);
+      } else {
+        step /= 2;
       }
-      int old_size = data.invalid.size();
-      data.invalid.resize(old_size + 1);
-      data.invalid[old_size].invalid_begin = start* 4;
-      data.invalid[old_size].nr_invalid = (end - start + 1) * 4; // nr_invalid is in bytes
+    }
+
+    SFXC_ASSERT(end - start + 1 > 0);
+    int old_size = data.invalid.size();
+    data.invalid.resize(old_size + 1);
+    data.invalid[old_size].invalid_begin = start * 4;
+    data.invalid[old_size].nr_invalid = (end - start + 1) * 4; // nr_invalid is in bytes
 #if 0
-      if (RANK_OF_NODE >= -1){
-        std::cout << RANK_OF_NODE << " : " << (end - start + 1) << " words of fill pattern found" << std::endl;
-      }
+    std::cout << RANK_OF_NODE << " : " << (end - start + 1) << " words of fill pattern found" << std::endl;
 #endif
-      start = end + 1;
-    }
+
+    start = end + 1;
   }
 }
