815d5c0a5d167b49b5d060536b46c394b2ad48fd
keimpema <keimpema@5736e3ea-d426-0410-8aaf-93e336ac3372>
2013-09-19 15:20:52 +0000

    VDIF print headers now checks if there is fill pattern in headers, also an option is added to override the frame size in the headers.
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@1644 5736e3ea-d426-0410-8aaf-93e336ac3372

--- utils/vdif_print_headers.cc
+++ utils/vdif_print_headers.cc
@@ -8,6 +8,7 @@
 #include <vector>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 #include "correlator_time.h"
 
 struct Header {
@@ -39,19 +40,50 @@ Time get_time(int ref_epoch, int sec_from_epoch){
   return time;
 }
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cout << "usage: " << argv[0] << " <vdif-file>" << std::endl;
-    return 1;
+void get_options(int argc, char*argv[], char **filename, int &size_frame){
+  int c, opt_index = 0;
+  static struct option long_options[] = {
+      {"help",    no_argument, 0, 'h'},
+      {"size_frame",    required_argument, 0, 's'},
+      {0, 0, 0, 0}
+  };
+  size_frame = 0;
+  while( (c = getopt_long (argc, argv, "hs:",  long_options, 
+                           &opt_index)) != -1){
+    switch (c){
+    case 'h':
+      std::cout << "usage: " << filename << "[-s frame_size] <vdif-file>\n";
+      exit(0);
+      break;
+    case 's':
+      sscanf(optarg, "%d", &size_frame);
+      break;
+    default:
+      printf ("option %s", long_options[opt_index].name);
+      abort();
+    }
   }
+  if ((argc-optind) != 1) {
+    std::cout << "usage: " << filename << "[-s frame_size] <vdif-file>\n";
+    exit(1);
+  }
+  *filename = argv[optind];
+}
+ 
+int main(int argc, char *argv[]) {
+  int size_frame;
+  char *filename;
+  get_options(argc, argv, &filename, size_frame);
 
-  FILE *infile = fopen(argv[1], "r");
+  FILE *infile = fopen(filename, "r");
   if(infile == NULL){
-    std::cout << "Could not open " << argv[1] << " for reading.\n";
+    std::cout << "Could not open " << filename << " for reading.\n";
     return 1;
   }
 
   bool eof = false;
+  int invalid_nr = 0;
+  size_t data_size = 1, header_size = 0;
   while (eof == false){
     Header header;
     unsigned char *header_buf = (unsigned char *) &header;
@@ -59,22 +91,42 @@ int main(int argc, char *argv[]) {
     if (header.legacy_mode == 0)
       nwords = fread(&header_buf[16],4,4,infile);
     if (nwords == 4){
-      Time t = get_time(header.ref_epoch, header.sec_from_epoch);
-      size_t data_size = 8*header.dataframe_length;
-      size_t header_size = (16+16*(1-header.legacy_mode));
-      uint16_t s_id = header.station_id;
-      char *s = (char *)&s_id;
-      char station[3];
-      station[0] = s[1];
-      station[1] = s[0];
-      station[2] = 0;
-      std::cout << t << " ,frame_nr = " << header.dataframe_in_second 
-                << ", thread_id = " << header.thread_id << ", nchan = " << (1 << header.log2_nchan)
-                << ", invalid = " << (int)header.invalid << ", legacy = " << (int)header.legacy_mode
-                << ", station = " << station
-                << ", bps-1 = " << (int) header.bits_per_sample
-                << ", data_size = " << data_size << "\n";
-      fseek(infile, data_size-header_size, SEEK_CUR);
+      if (((uint32_t *)header_buf)[0] == 0x11223344 ||
+          ((uint32_t *)header_buf)[1] == 0x11223344 ||
+          ((uint32_t *)header_buf)[2] == 0x11223344 ||
+          ((uint32_t *)header_buf)[3] == 0x11223344){
+        std::cout << "Invalid frame with fill pattern : Nr of previous bad frames = " << invalid_nr << "\n";
+        invalid_nr++;
+      }else{
+        Time t = get_time(header.ref_epoch, header.sec_from_epoch);
+        data_size = 8*header.dataframe_length;
+        header_size = (16+16*(1-header.legacy_mode));
+        uint16_t s_id = header.station_id;
+        char *s = (char *)&s_id;
+        char station[3];
+        if (isalnum(s[0]) && isalnum(s[1])){
+          station[0] = s[1];
+          station[1] = s[0];
+          station[2] = 0;
+        }else{
+          station[0] = ' ';
+          station[1] = ' ';
+          station[2] = 0;
+        }
+        invalid_nr = 0;
+        std::cout << t << " ,frame_nr = " << header.dataframe_in_second 
+                  << ", thread_id = " << header.thread_id << ", nchan = " << (1 << header.log2_nchan)
+                  << ", invalid = " << (int)header.invalid << ", legacy = " << (int)header.legacy_mode
+                  << ", station = " << station
+                  << ", bps-1 = " << (int) header.bits_per_sample
+                  << ", data_size = " << data_size << "\n";
+      }
+      if (size_frame > 0){
+        // The frame size in the header is overridden
+        fseek(infile, size_frame-header_size, SEEK_CUR);
+      }else{
+        fseek(infile, data_size-header_size, SEEK_CUR);
+      }
     } else {
       eof = true;
     }
