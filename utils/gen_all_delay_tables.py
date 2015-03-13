70cfc6a87616a5c01a3144d4b980c9bf0e1a415f
keimpema <keimpema@5736e3ea-d426-0410-8aaf-93e336ac3372>
2015-03-13 10:12:34 +0000

    Let the gen_all_delay_tables script compute the delays in parallel rather than sequentialy
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@1868 5736e3ea-d426-0410-8aaf-93e336ac3372

--- utils/gen_all_delay_tables.py
+++ utils/gen_all_delay_tables.py
@@ -6,7 +6,9 @@
 # Author(s): Aard Keimpema <keimpema@jive.nl>
 # 
 
-import sys, os, time, filecmp
+import sys, os
+import subprocess
+from optparse import OptionParser
 
 # The json module is new in Python 2.6; fall back on simplejson if it
 # isn't available.
@@ -16,7 +18,7 @@ except:
     import simplejson as json
     pass
 
-def gen_table(vex_file, ctrl_file):
+def gen_delay_tables(vex_file, ctrl_file, nprocs):
   if not os.path.exists(vex_file):
     print "Vex file does not exist : " + vex_file
     sys.exit(1)
@@ -47,22 +49,53 @@ def gen_table(vex_file, ctrl_file):
     print "Control file does not have a stations field"
     exit(0)
   experiment_name = ctrl["exper_name"]
-  
-  for s in ctrl["stations"]:
-    delay_file = delay_directory + "/" + experiment_name + "_" + s + ".del"
-    cmd = "generate_delay_model " + vex_file + " " + s + " " + delay_file
-    print "Generating delay model for station " + s
-    if(os.system(cmd)!=0):
-      print "Error executing command : " + cmd
-      sys.exit(1)
+  nstations = len(ctrl["stations"])
+  if nprocs == None:
+    nprocs = nstations
+  else:
+    nprocs = min(nstations, nprocs)
+  procs = [(None, None) for i in range(nprocs)]
+  for i in range(nprocs):
+    station = ctrl["stations"][i]
+    procs[i] = (station, gen_delay(delay_directory, experiment_name, vex_file, station))
+  for i in range(nprocs, nstations):
+    station = ctrl["stations"][i]
+    done = False
+    while not done:
+      for j in range(nprocs):
+        status = procs[j][1].poll()
+        if status != None:
+          if status < 0:
+            print "Error failed generating delay file for station", procs[j][0]
+            sys.exit(1)
+          else:
+            done = True
+            procs[j] = (station, gen_delay(delay_directory, experiment_name, vex_file, station))
+            break
+  # Wait for processes to finish
+  for j in range(nprocs):
+    status = procs[j][1].wait()
+    if status < 0:
+      print "Error failed generating delay file for station", procs[j][0]
+
+def gen_delay(delay_directory, experiment_name, vex_file, station):
+  delay_file = delay_directory + "/" + experiment_name + "_" + station + ".del"
+  cmd = ["generate_delay_model", vex_file, station , delay_file]
+  print cmd
+  proc = subprocess.Popen(cmd)
+  print "Generating delay model for station " + station
+  return proc
 
 
 ######### MAIN CODE
 if __name__ == "__main__":
-  if len(sys.argv) == 3:
-    gen_table(sys.argv[1], sys.argv[2])
+  usage = "Usage: %prog [options] <vex file> <control file>"
+  parser = OptionParser(usage=usage)
+  parser.add_option("-p", "--number-process", dest="nprocs", 
+                    help="Specify the number of generate jobs to be run simultaneously",
+                    action="store", type="int")
+  opts, args = parser.parse_args()
+  if len(args) == 2:
+    gen_delay_tables(args[0], args[1], opts.nprocs)
   else:
-    print "Usage", sys.argv[0], " <vex-file> <ctrl-file>"
-    sys.exit(1)
-
-  sys.exit(0)
+    parser.error("Invalid number of arguments")
