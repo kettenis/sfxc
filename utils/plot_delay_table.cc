b24c1d29408421a8ba3770bb81cfce0e85202cb2
keimpema <keimpema@5736e3ea-d426-0410-8aaf-93e336ac3372>
2015-03-16 15:59:02 +0000

    Make the number of points which plot_delat_table prints configurable
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@1870 5736e3ea-d426-0410-8aaf-93e336ac3372

--- utils/plot_delay_table.cc
+++ utils/plot_delay_table.cc
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <getopt.h>
 
 #include "utils.h"
 #include "delay_table_akima.h"
 #include "control_parameters.h"
 
+void usage(const char *name){
+  std::cout << "Usage: " << name << " [options] <delay-table> <plot-file>\n";
+  std::cout << "Options : -h, --help                  Print this help message\n";
+  std::cout << "          -n, --nr-interpolate        The number of interpolated points to\n"
+            << "                                      print between two rows of the delay\n"
+            << "                                      table\n";
+}
+
 //converts a binary delay table to an interpolated ascii table
-//Usage: plot_delay_table <delay-table> <plot-file>
+//Usage: plot_delay_table [options] <delay-table> <plot-file>
 //
 //each row contains a value for the time and the delay. 
 //Scans are separated with an extra newline, which makes gnuplot draw a new curve.
@@ -14,17 +23,55 @@ int main(int argc, char *argv[]) {
 #ifdef SFXC_PRINT_DEBUG
   RANK_OF_NODE = 0;
 #endif
+  int n_interpol = 9;
+  int c;
 
-  if (argc != 3) {
-    std::cout << "usage: " << argv[0] << " <delay-table> <plot-file>"
-    << std::endl;
+  while (1)
+    {
+      static struct option long_options[] =
+        {
+          {"help",    no_argument,       0, 'h'},
+          {"number-points",    required_argument, 0, 'n'},
+          {0, 0, 0, 0}
+        };
+      int option_index = 0;
+
+      c = getopt_long (argc, argv, "hn:",
+                       long_options, &option_index);
+
+      /* Detect the end of the options. */
+      if (c == -1)
+        break;
+
+      switch (c)
+        {
+        case 'h':
+          usage(argv[0]);
+          exit(0);
+          break;
+        case 'n':
+          n_interpol = atoi(optarg);
+          break;
+        case '?':
+          exit(1);
+          break;
+
+        default:
+          usage(argv[0]);
+          exit(1);
+        }
+    }
+
+  if(argc-optind != 2){
+    std::cout << "Error, invalid number of arguments!\n\n";
+    usage(argv[0]);
     exit(1);
   }
 
   Delay_table delay_table;
-  delay_table.open(argv[1]);
+  delay_table.open(argv[optind]);
   delay_table.set_clock_offset(0., 0., 0., 0.);
-  std::ofstream out(argv[2]);
+  std::ofstream out(argv[optind+1]);
   out.precision(20);
 
   int scan = 1;
@@ -39,14 +86,18 @@ int main(int argc, char *argv[]) {
               << " \t" << start_time_scan
               << " \t" << stop_time_scan
               << std::endl;
-    Time step = Time(100000.);
-    for (Time time = start_time_scan; time < stop_time_scan; time += step) {
-      out << time << ", usec = " << (int64_t)time.get_time_usec();
-      for(int j = 0 ; j < akima.n_phase_centers(); j++)
-        out << " \t(" << akima.delay(time, j) 
-            << ", " << akima.phase(time, j)
-            << ", " << akima.amplitude(time, j) << ")";
-      out << std::endl;
+    Time onesec = Time(1000000.);
+    Time step = Time(1000000./(n_interpol+1));
+    for (Time t0 = start_time_scan; t0 < stop_time_scan; t0 += onesec) {
+      for (int i=0;i<n_interpol+1;i++){
+        Time time = t0 + step*i;
+        out << time << ", usec = " << (int64_t)time.get_time_usec();
+        for(int j = 0 ; j < akima.n_phase_centers(); j++)
+          out << " \t(" << akima.delay(time, j) 
+              << ", " << akima.phase(time, j)
+              << ", " << akima.amplitude(time, j) << ")";
+        out << std::endl;
+      }
     }
     out << std::endl;
     scan ++;
