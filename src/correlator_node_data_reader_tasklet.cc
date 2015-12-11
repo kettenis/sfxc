bcb88685cecc2c031406012f6030305984bbfedb
kettenis <kettenis@5736e3ea-d426-0410-8aaf-93e336ac3372>
2015-12-11 12:43:00 +0000

    Bump the size of the memory pool.  We have been bumping this with local modifications since forever.  The new value should be enough to keep things streaming for 32 MHz bands with 4 second integrations.
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@1929 5736e3ea-d426-0410-8aaf-93e336ac3372

--- src/correlator_node_data_reader_tasklet.cc
+++ src/correlator_node_data_reader_tasklet.cc
@@ -2,7 +2,7 @@
 
 Correlator_node_data_reader_tasklet::
 Correlator_node_data_reader_tasklet()
-    : input_buffer(18100000), bytes_left(0),
+  : input_buffer(37100000), bytes_left(0),
     new_stream_available(false),state(IDLE) {
 }
 
