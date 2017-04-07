3ed3ec516a8a41d8861cb6dc13cc20021739d72e
kettenis <kettenis@5736e3ea-d426-0410-8aaf-93e336ac3372>
2017-04-07 10:05:45 +0000

    Style.
    git-svn-id: svn+ssh://jop91.astron.nl/auto/home/jive_cvs/SOFTC/svn/trunk/sfxc@2014 5736e3ea-d426-0410-8aaf-93e336ac3372

--- src/delay_table_akima.cc
+++ src/delay_table_akima.cc
@@ -29,36 +29,35 @@
 #include <fstream>
 #include <string>
 
-#define READ_SCAN_HEADER  0
-#define FIND_TSTART  1
-#define START_NEW_SCAN 2
-#define READ_PHASE_CENTER 3
-
+#define READ_SCAN_HEADER	0
+#define FIND_TSTART		1
+#define START_NEW_SCAN		2
+#define READ_PHASE_CENTER	3
 
 //*****************************************************************************
 //function definitions
 //*****************************************************************************
 
-Delay_table_akima::Delay_table_akima(){
+Delay_table_akima::Delay_table_akima() {
   ref_count = new int(1);
   mutex = new pthread_mutex_t;
   pthread_mutex_init(mutex, NULL);
 }
 
-Delay_table_akima::~Delay_table_akima(){
+Delay_table_akima::~Delay_table_akima() {
   free_reference();
 }
 
-Delay_table_akima::Delay_table_akima(const Delay_table_akima &other){
+Delay_table_akima::Delay_table_akima(const Delay_table_akima &other) {
   copy(other);
 }
 
 void
-Delay_table_akima::free_reference(){
+Delay_table_akima::free_reference() {
   pthread_mutex_lock(mutex);
   *ref_count -= 1;
-  if(*ref_count == 0){
-    for(int i = 0 ; i < splineakima.size() ; i++){
+  if (*ref_count == 0) {
+    for (int i = 0 ; i < splineakima.size(); i++) {
       gsl_spline_free(splineakima[i]);
       gsl_spline_free(splineakima_ph[i]);
       gsl_spline_free(splineakima_amp[i]);
@@ -70,7 +69,7 @@ Delay_table_akima::free_reference(){
     pthread_mutex_destroy (mutex);
     delete mutex;
     delete ref_count;
-  } else{
+  } else {
    pthread_mutex_unlock(mutex);
   }
   sources.resize(0);
@@ -83,7 +82,7 @@ Delay_table_akima::free_reference(){
 }
 
 void
-Delay_table_akima::copy(const Delay_table_akima &other){
+Delay_table_akima::copy(const Delay_table_akima &other) {
   pthread_mutex_lock(other.mutex);
   mutex = other.mutex;
   ref_count = other.ref_count;
@@ -142,11 +141,10 @@ double Delay_table_akima::accel(const Time &time, int phase_center) {
 
   double sec = time.diff(scan_begin);
   double result = gsl_spline_eval_deriv2(splineakima[phase_center], sec, acc[phase_center]);
-  // FIXME: Why is the clock rate added to the acceleration?
-  return result + clock_rate;
+  return result;
 }
 
-double Delay_table_akima::phase(const Time &time, int phase_center){
+double Delay_table_akima::phase(const Time &time, int phase_center) {
   SFXC_ASSERT(time >= interval_begin);
   SFXC_ASSERT(time <= interval_end);
   SFXC_ASSERT(splineakima.size() > phase_center);
@@ -156,7 +154,7 @@ double Delay_table_akima::phase(const Time &time, int phase_center){
   return result;
 }
 
-double Delay_table_akima::amplitude(const Time &time, int phase_center){
+double Delay_table_akima::amplitude(const Time &time, int phase_center) {
   SFXC_ASSERT(time >= interval_begin);
   SFXC_ASSERT(time <= interval_end);
   SFXC_ASSERT(splineakima.size() > phase_center);
@@ -175,7 +173,7 @@ Delay_table::Delay_table()
 
 // Copy constructor
 Delay_table::Delay_table(const Delay_table &other)
-  : scan_nr(0), clock_nr(0), n_sources_in_current_scan(0){
+  : scan_nr(0), clock_nr(0), n_sources_in_current_scan(0) {
   clock_starts = other.clock_starts;
   clock_offsets = other.clock_offsets;
   clock_rates = other.clock_rates;
@@ -209,21 +207,23 @@ void Delay_table::operator=(const Delay_table &other) {
 }
 
 bool Delay_table::operator==(const Delay_table &other) const {
-  if(scans.size() != other.scans.size()) return false;
-  for(int i = 0 ; i < delays.size() ; i++){
-    if (delays[i] != other.delays[i]) return false;
+  if (scans.size() != other.scans.size())
+    return false;
+  for (int i = 0 ; i < delays.size(); i++) {
+    if (delays[i] != other.delays[i])
+      return false;
   }
   return true;
 }
 
 // Set clock offset and rate
-void Delay_table::set_clock_offset(const double offset, const Time start, const double rate, const Time epoch){
+void Delay_table::set_clock_offset(const double offset, const Time start, const double rate, const Time epoch) {
   clock_starts.push_back(start);
   clock_offsets.push_back(offset);
   clock_rates.push_back(rate);
   clock_epochs.push_back(epoch);
   // We don't do clock jumps in the middle of a scan
-  if((scans.size() > 0) && (start <= scans[scan_nr].begin))
+  if (scans.size() > 0 && start <= scans[scan_nr].begin)
     clock_nr = clock_starts.size() - 1;
 }
 
@@ -234,18 +234,18 @@ void Delay_table::open(const char *delayTableName) {
   open(delayTableName, start, stop);
 }
 
-void Delay_table::open(const char *delayTableName, const Time tstart, const Time tstop){
+void Delay_table::open(const char *delayTableName, const Time tstart, const Time tstop) {
   std::ifstream in(delayTableName);
-  if(!in.is_open())
+  if (!in.is_open())
     sfxc_abort((std::string("Could not open delay table ")+std::string(delayTableName)).c_str());
 
   int32_t header_size;
   // Read the header
-  in.read(reinterpret_cast < char * > (&header_size), sizeof(int32_t));
+  in.read(reinterpret_cast<char *>(&header_size), sizeof(int32_t));
   if (in.eof()) return;
 
   char header[header_size];
-  in.read(reinterpret_cast < char * > (header), header_size*sizeof(char));
+  in.read(reinterpret_cast<char *>(header), header_size * sizeof(char));
   if (in.eof()) return;
 
   double line[7], scan_start, scan_end;
@@ -255,39 +255,38 @@ void Delay_table::open(const char *delayTableName, const Time tstart, const Time
   int correlation_scan = 0;
   int state = READ_SCAN_HEADER;
   bool done_reading = false;
-  while((!done_reading) && (in.good())){
-    switch(state){
+  while (!done_reading && in.good()) {
+    switch (state) {
     case READ_SCAN_HEADER:{
-      if(in.read(current_source, sizeof(char) * 81)){
-        in.read(reinterpret_cast < char * > (&current_mjd), sizeof(int32_t));
-        in.read(reinterpret_cast < char * > (line), 7*sizeof(double));
+      if (in.read(current_source, sizeof(char) * 81)) {
+        in.read(reinterpret_cast<char *>(&current_mjd), sizeof(int32_t));
+        in.read(reinterpret_cast<char *>(line), 7 * sizeof(double));
         Time start_time_scan(current_mjd, line[0]);
         // strip whitespace from end of source string
-        for(int i = 79; i >=0 ; i--){
-          if(current_source[i] != ' '){
+        for (int i = 79; i >= 0; i--) {
+          if (current_source[i] != ' ') {
             current_source[i + 1] = 0;
             break;
           }
         }
-        if(start_time_scan < tstart){
+        if (start_time_scan < tstart) {
           state = FIND_TSTART;
-        } else if (start_time_scan >= tstop){
+        } else if (start_time_scan >= tstop) {
           done_reading = true;
-        }else {
+        } else {
           state = START_NEW_SCAN;
         }
       }
       break;
     }
     case FIND_TSTART:{
-      while (in.read(reinterpret_cast < char * > (line), 7*sizeof(double))) {
-
+      while (in.read(reinterpret_cast<char *>(line), 7 * sizeof(double))) {
         Time time(current_mjd, line[0]);
 
-        if(line[0] == 0 && line[4] == 0){
+        if (line[0] == 0 && line[4] == 0) {
           state = READ_SCAN_HEADER;
           break;
-        }else if(time >= tstart){
+        } else if (time >= tstart) {
           state = START_NEW_SCAN;
           break;
         }
@@ -299,11 +298,11 @@ void Delay_table::open(const char *delayTableName, const Time tstart, const Time
       Time start_time_scan(current_mjd, scan_start);
       // look up the current source in the list of sources and append it to the list if needed
       int source_index;
-      for(source_index = 0; source_index < sources.size() ; source_index++){
-        if(sources[source_index] == current_source)
+      for (source_index = 0; source_index < sources.size(); source_index++) {
+        if (sources[source_index] == current_source)
           break;
       }
-      if(source_index == sources.size())
+      if (source_index == sources.size())
         sources.push_back(current_source);
 
       // Create the new scan
@@ -316,7 +315,7 @@ void Delay_table::open(const char *delayTableName, const Time tstart, const Time
       scan.amplitudes = amplitudes.size();
       // Check if we are correlating an additional phase center to the current scan
       int n_scans = scans.size();
-      if((n_scans > 1) && (scans[n_scans - 2].begin == start_time_scan)){
+      if (n_scans > 1 && scans[n_scans - 2].begin == start_time_scan) {
         // We found an additional phace center to the current scan, overwrite previous times
         times.resize(scans[n_scans - 2].times);
       }
@@ -329,12 +328,12 @@ void Delay_table::open(const char *delayTableName, const Time tstart, const Time
       // Read the data
       do {
         if (line[0] == 0 && line[4] == 0) {
-          if(times.size() == 1){
+          if (times.size() == 1) {
             // Instead of the first point of the desired scan, we got the
             // last point of the previous scan.  Get rid of it.
             scans.resize(0);
             times.resize(0);
-          }else{
+          } else {
             SFXC_ASSERT(scan_end > scan_start);
             scan.end.set_time(current_mjd, scan_end);
           }
@@ -347,7 +346,7 @@ void Delay_table::open(const char *delayTableName, const Time tstart, const Time
           amplitudes.push_back(line[6]);
           scan_end = line[0];
         }
-      } while(in.read(reinterpret_cast < char * > (line), 7*sizeof(double)));
+      } while (in.read(reinterpret_cast<char *>(line), 7 * sizeof(double)));
       correlation_scan = scans.size() - 1;
       break;
     }}
@@ -399,13 +398,13 @@ bool Delay_table::initialise_next_scan() {
 
   scan_nr += n_sources_in_previous_scan;
   // Check scan length (need at least 5 points)
-  if(scans[scan_nr].end.diff(scans[scan_nr].begin) < 4.)
+  if (scans[scan_nr].end.diff(scans[scan_nr].begin) < 4.)
     return false;
 
   // Determine the number of sources in current scan
   n_sources_in_current_scan = 1;
-  while((scan_nr + n_sources_in_current_scan < scans.size()) &&
-        (scans[scan_nr + n_sources_in_current_scan - 1].begin == scans[scan_nr + n_sources_in_current_scan].begin))
+  while (scan_nr + n_sources_in_current_scan < scans.size() &&
+	 scans[scan_nr + n_sources_in_current_scan - 1].begin == scans[scan_nr + n_sources_in_current_scan].begin)
     n_sources_in_current_scan++;
   clock_nr = clock_starts.size() - 1;
   while (clock_nr > 0 && clock_starts[clock_nr] > scans[scan_nr].begin)
@@ -415,18 +414,18 @@ bool Delay_table::initialise_next_scan() {
 }
 
 Delay_table_akima
-Delay_table::create_akima_spline(const Time start_, const Time duration){
+Delay_table::create_akima_spline(const Time start_, const Time duration) {
   // Round begin and end time to integer second
-  int mjd_start = (int) floor(start_.get_mjd());
+  int mjd_start = (int)floor(start_.get_mjd());
   double seconds_start = floor(start_.get_time_usec() / 1000000);
   Time stop = start_ + duration;
-  int mjd_stop = (int) floor(stop.get_mjd());
+  int mjd_stop = (int)floor(stop.get_mjd());
   double seconds_stop = ceil(stop.get_time_usec() / 1000000);
   Time start = Time(mjd_start, seconds_start);
-  int n_seconds = (mjd_stop - mjd_start)*86400 + seconds_stop - seconds_start;
+  int n_seconds = (mjd_stop - mjd_start) * 86400 + seconds_stop - seconds_start;
   // Move to the correct scan
-  while(stop > scans[scan_nr].end){
-    if (!initialise_next_scan()){
+  while (stop > scans[scan_nr].end) {
+    if (!initialise_next_scan()) {
       std::string msg = "Error: time " + start.date_string() + " is not in delay table";
       sfxc_abort(msg.c_str());
     }
@@ -434,17 +433,17 @@ Delay_table::create_akima_spline(const Time start_, const Time duration){
 
   // Determine interpolation interval
   Time onesec(1000000.);
-  Time interval_begin = start - onesec*2;
-  Time interval_end = start + onesec*(2+n_seconds);
-  if(interval_begin < scans[scan_nr].begin){
+  Time interval_begin = start - onesec * 2;
+  Time interval_end = start + onesec * (2 + n_seconds);
+  if (interval_begin < scans[scan_nr].begin) {
     interval_begin = scans[scan_nr].begin;
     if (interval_end.diff(interval_begin) < 4)
-      interval_end = interval_begin + onesec*4;
+      interval_end = interval_begin + onesec * 4;
   }
-  if(interval_end > scans[scan_nr].end){
+  if (interval_end > scans[scan_nr].end) {
     interval_end = scans[scan_nr].end;
     if (interval_end.diff(interval_begin) < 4)
-      interval_begin = interval_end - onesec*4;
+      interval_begin = interval_end - onesec * 4;
   }
 
   // Create the splines
@@ -457,7 +456,7 @@ Delay_table::create_akima_spline(const Time start_, const Time duration){
   result.acc_amp.resize(n_sources_in_current_scan);
   result.sources.resize(n_sources_in_current_scan);
   SFXC_ASSERT(n_sources_in_current_scan > 0);
-  for(int i = 0; i < n_sources_in_current_scan ; i++){
+  for (int i = 0; i < n_sources_in_current_scan; i++) {
     Scan &scan = scans[scan_nr + i];
     result.sources[i] = sources[scan.source];
 
@@ -492,22 +491,23 @@ Time Delay_table::start_time_scan() {
   SFXC_ASSERT(scan_nr < scans.size());
   return scans[scan_nr].begin;
 }
+
 Time Delay_table::stop_time_scan() {
   SFXC_ASSERT(scan_nr < scans.size());
   return scans[scan_nr].end;
 }
 
-bool Delay_table::goto_scan(const Time &time){
-  if((time >= start_time_scan()) && (time < stop_time_scan()))
+bool Delay_table::goto_scan(const Time &time) {
+  if (time >= start_time_scan() && time < stop_time_scan())
     return true;
 
-  if(time < start_time_scan())
+  if (time < start_time_scan())
     scan_nr = 0;
 
   n_sources_in_current_scan = 0;
-  while ((scan_nr < scans.size()) && (time > scans[scan_nr].end)) 
+  while (scan_nr < scans.size() && time > scans[scan_nr].end)
     scan_nr++;
-  if(scan_nr < scans.size()){
+  if (scan_nr < scans.size()) {
     initialise_next_scan();
     return true;
   }
